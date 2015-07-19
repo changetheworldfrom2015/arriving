e851a2dc9d173597687b52ea6df430350f896887
Qiucen Zhang <qzhangprinceton@gmail.com>
2015-07-18 23:13:07 -0400

    add 4 main fragments, the radio button selection works now

--- app/src/main/java/com/changetheworldfrom2015/arriving/MainActivity.java
+++ app/src/main/java/com/changetheworldfrom2015/arriving/MainActivity.java
@@ -6,7 +6,9 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.parse.ParseUser;
@@ -18,6 +20,8 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init_view();
 
         findViewById(R.id.logout_button).setOnClickListener(new View.OnClickListener() {
             @TargetApi(Build.VERSION_CODES.HONEYCOMB)
@@ -36,6 +40,33 @@ public class MainActivity extends FragmentActivity {
                 } else {
                     finish();
                 }
+            }
+        });
+    }
+
+    public void init_view(){
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, new EventsFragment()).commit();
+        RadioGroup myTabRg = (RadioGroup) findViewById(R.id.tab_menu);
+        myTabRg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.rbEvents:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, new EventsFragment()).commit();
+                        break;
+                    case R.id.rbContacts:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, new ContactsFragment()).commit();
+                        break;
+                    case R.id.rbSettings:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, new SettingsFragment()).commit();
+                        break;
+                    case R.id.rbMore:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, new MoreFragment()).commit();
+                        break;
+                    default:
+                        break;
+                }
+
             }
         });
     }
